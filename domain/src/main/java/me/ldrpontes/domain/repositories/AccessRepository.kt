7b41422aef451816a10a698fd25bcd79c3e6f945
Leandro Pontes Berleze <leandropberleze@gmail.com>
2020-12-14 19:37:26 -0300

    Name refactor

--- domain/src/main/java/me/ldrpontes/domain/repositories/AccessRepository.kt
+++ domain/src/main/java/me/ldrpontes/domain/repositories/AccessRepository.kt
@@ -3,6 +3,6 @@ package me.ldrpontes.domain.repositories
 import me.ldrpontes.domain.entities.Access
 import me.ldrpontes.domain.entities.Result
 
-interface AuthRepository {
+interface AccessRepository {
     suspend fun doLogin(email: String, password: String): Result<Access>
 }
