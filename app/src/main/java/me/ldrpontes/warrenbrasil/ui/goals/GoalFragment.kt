2a8e8faeb231107b45f4a9103a028d4c5f739c20
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-01-13 21:40:36 -0300

    App layer layout implementation

---
+++ app/src/main/java/me/ldrpontes/warrenbrasil/ui/goals/GoalFragment.kt
@@ -0,0 +1,18 @@
+package me.ldrpontes.warrenbrasil.ui.goals
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.ldrpontes.warrenbrasil.R
+
+class GoalFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_goal, container, false)
+    }
+}
--- app/src/main/java/me/ldrpontes/warrenbrasil/ui/login/LoginActivity.kt
+++ app/src/main/java/me/ldrpontes/warrenbrasil/ui/login/LoginActivity.kt
@@ -1,10 +1,13 @@
 package me.ldrpontes.warrenbrasil.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_login.*
 import me.ldrpontes.warrenbrasil.R
 import me.ldrpontes.warrenbrasil.databinding.ActivityLoginBinding
+import me.ldrpontes.warrenbrasil.ui.goals.GoalActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,6 +18,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener {
+            startActivity(Intent(this, GoalActivity::class.java))
+        }
     }
 }
 
