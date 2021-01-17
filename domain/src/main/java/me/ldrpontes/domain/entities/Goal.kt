fbf78c9fb86423b20804c30d61ad7b44db43e9f0
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-01-17 20:29:15 -0300

    Aux method

--- domain/src/main/java/me/ldrpontes/domain/entities/Goal.kt
+++ domain/src/main/java/me/ldrpontes/domain/entities/Goal.kt
@@ -1,3 +1,11 @@
 package me.ldrpontes.domain.entities
 
-data class Goal(val id: String, var name: String, var totalBalance: Double, var goalAmount: Double?, var goalDate: String, var background: Background)
+data class Goal(val id: String, var name: String, var totalBalance: Double, var goalAmount: Double?, var goalDate: String, var background: Background) {
+    fun isGoalCompleted(): Boolean {
+        return if(goalAmount != null){
+            totalBalance >= goalAmount!!
+        } else {
+            false
+        }
+    }
+}
